0e26d6037ff62a55478ff108e162bd0af95928cf
pouya <pouyarezaei810@gmail.com>
2021-08-03 21:04:52 +0430

    create voice search page widget

---
+++ lib/ui/page/voice_search.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class VoiceSearch extends StatelessWidget {
+  const VoiceSearch({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("ترجمه صوتی"),
+      ),
+    );
+  }
+}
