9fa44933898d763a5831009279b097f94facacb2
pouya <pouyarezaei810@gmail.com>
2021-08-03 21:04:37 +0430

    create word search page widget

---
+++ lib/ui/page/word_search.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WordSearch extends StatelessWidget {
+  const WordSearch({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Text("ترجمه لغت"),),
+    );
+  }
+}
