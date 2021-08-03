e0b5e6e16c1daff0883b16b3e7e4c1fe1b5a284f
pouya <pouyarezaei810@gmail.com>
2021-08-03 21:08:43 +0430

    create history page

---
+++ lib/ui/page/history.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SearchHistory extends StatelessWidget {
+  const SearchHistory({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("تاریخچه"),
+      ),
+    );
+  }
+}
