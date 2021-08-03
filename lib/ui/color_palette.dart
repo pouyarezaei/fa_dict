f645529b3f3d6ebe98324548f61490be0c5c0349
pouya <pouyarezaei810@gmail.com>
2021-08-03 21:07:55 +0430

    create color_palette

---
+++ lib/ui/color_palette.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// Color color = new Color(0xffe7e7e7);
+// Color color1 = new Color(0xffe7e7e7);
+// Color color2 = new Color(0xffbbc7c5);
+// Color color3 = new Color(0xff080827);
+// Color color4 = new Color(0xff6a6b8d);
+// Color color5 = new Color(0xff3f4165);
+// Color color6 = new Color(0xffce803c);
+Color wildWatermelon = new Color(0xffFF5273);
+Color pattensBlue = new Color(0xffD8EFFF);
+Color mandy = new Color(0xffe75e82);
+Color buccaneer = new Color(0xff6F2F3B);
+Color bilobaFlower = new Color(0xffb2aff0);
+Color vanillaIce = new Color(0xffEFD2DE);
+Color malachite = new Color(0xff3cdf37);
