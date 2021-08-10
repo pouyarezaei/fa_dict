15fe56d0d2bd9c9a2164de62ba108fac22cf7b99
pouya <pouyarezaei810@gmail.com>
2021-08-10 15:51:45 +0430

    add theme mode setting

--- lib/ui/page/setting.dart
+++ lib/ui/page/setting.dart
@@ -1,14 +1,59 @@
+import 'package:fa_dict/bloc/theme/theme_bloc.dart';
+import 'package:fa_dict/ui/color_palette.dart';
+import 'package:fa_dict/ui/widget/title_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class Setting extends StatelessWidget {
+class Setting extends StatefulWidget {
   const Setting({Key? key}) : super(key: key);
+
+  @override
+  _SettingState createState() => _SettingState();
+}
+
+class _SettingState extends State<Setting> {
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text("تنظیمات"),
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: BlocBuilder<ThemeBloc, ThemeState>(
+        builder: (context, state) => Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TitleWidget(title: "مود"),
+            ListTile(
+              title: const Text('سیستم'),
+              leading: Radio<ThemeMode>(
+                  activeColor: ColorPalette.wildWatermelon,
+                  value: ThemeMode.system,
+                  groupValue: state.themeMode,
+                  onChanged: _onThemeChanged),
+            ),
+            ListTile(
+              title: const Text('تاریک'),
+              leading: Radio<ThemeMode>(
+                  activeColor: ColorPalette.wildWatermelon,
+                  value: ThemeMode.dark,
+                  groupValue: state.themeMode,
+                  onChanged: _onThemeChanged),
+            ),
+            ListTile(
+              title: const Text('روشن'),
+              leading: Radio<ThemeMode>(
+                  activeColor: ColorPalette.wildWatermelon,
+                  value: ThemeMode.light,
+                  groupValue: state.themeMode,
+                  onChanged: _onThemeChanged),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  _onThemeChanged(ThemeMode? value) {
+    BlocProvider.of<ThemeBloc>(context).add(ThemeChanged(value));
   }
 }
