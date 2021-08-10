530004a6925ce2bb32a5ff9da5a99a005dfd11ed
pouya <pouyarezaei810@gmail.com>
2021-08-10 15:53:04 +0430

    append loading stored theme logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,50 @@
 import 'package:fa_dict/bloc/database_builder/database_builder_bloc.dart';
-import 'package:fa_dict/ui/color_palette.dart';
+import 'package:fa_dict/bloc/theme/theme_bloc.dart';
+import 'package:fa_dict/ui/app_theme.dart';
 import 'package:fa_dict/ui/page/entry_point_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  int themeIndex = prefs.getInt(AppTheme.themeKey) ?? 0;
+  runApp(BlocProvider(
+    create: (_) => ThemeBloc(ThemeMode.values[themeIndex]),
+    child: App(),
+  ));
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primaryColor: ColorPalette.wildWatermelon, fontFamily: 'IranSans'),
-      home: MultiBlocProvider(
-        providers: [
-          BlocProvider<DatabaseBuilderBloc>(
-            create: (_) => DatabaseBuilderBloc(),
-          ),
-        ],
-        child: Directionality(
-            textDirection: TextDirection.rtl, child: EntryPointWidget()),
-      ),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<DatabaseBuilderBloc>(
+          create: (_) => DatabaseBuilderBloc(),
+        ),
+      ],
+      child: BlocBuilder<ThemeBloc, ThemeState>(
+          builder: (context, state) => MaterialApp(
+                debugShowCheckedModeBanner: false,
+                themeMode: state.themeMode,
+                theme: AppTheme.lightTheme,
+                darkTheme: AppTheme.darkTheme,
+                home: Directionality(
+                    textDirection: TextDirection.rtl,
+                    child: EntryPointWidget()),
+              )),
     );
   }
 }
